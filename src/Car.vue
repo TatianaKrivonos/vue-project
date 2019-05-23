7fad42b5e05f1b3c58f559605e26ee32755c21bc
TatianaKrivonos <konan_air.96@mail.ru>
2019-05-23 22:49:23 +0300

    создание компонента Car и передача ему параметров

---
+++ src/Car.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="car">
+    <h3>Name: {{ carName }}</h3>
+    <p>Year: {{carYear}} </p>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['carName', 'carYear'],
+  }
+</script>
+<style>
+  .car {
+    padding: 10px;
+    border: 1px solid #000;
+  }
+
+  .car h3 {
+    margin-bottom: 10px;
+  }
+</style>
