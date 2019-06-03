6267a6de02c9f3274fd72744e53230c543663efa
TatianaKrivonos <konan_air.96@mail.ru>
2019-06-03 21:14:17 +0300

    создание навигации (router-link)

--- src/App.vue
+++ src/App.vue
@@ -102,7 +102,19 @@
         >Submit</button>
       </form>
     </div> -->
-    <router-view></router-view>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/cars">Cars</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <router-view></router-view>
   </div>
 </template>
 
